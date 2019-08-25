01874eb6a8988bc456c6b4fc5c80e2939feecc6e
David Piper <black.god1930@gmail.com>
2019-08-25 05:17:53 -0400

    Updates

---
+++ precache-manifest.6e0b0950e27a0bd9ce15abab81680e90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fcea988051e1b1f686aa4fd149a0d6",
+    "url": "/william-setstatespear/index.html"
+  },
+  {
+    "revision": "4eb6ba73218cd37bbffa",
+    "url": "/william-setstatespear/static/css/main.950e390b.chunk.css"
+  },
+  {
+    "revision": "e13a71d7ee05cff05e6e",
+    "url": "/william-setstatespear/static/js/2.e4fd5714.chunk.js"
+  },
+  {
+    "revision": "4eb6ba73218cd37bbffa",
+    "url": "/william-setstatespear/static/js/main.0126463c.chunk.js"
+  },
+  {
+    "revision": "3c8b6602ce9d86672e28",
+    "url": "/william-setstatespear/static/js/runtime~main.c41fb1f9.js"
+  }
+]);
